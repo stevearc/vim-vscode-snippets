1558c5fcb365c956c61aa8b8199a48cf8fb8a46d
Steven Arcangeli <stevearc@stevearc.com>
2021-01-28 10:33:30 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # vim-vsnip-snippets
 A collection of vscode snippets for vim
+
+> Why does this project exist?
+
+Inspired by [vim-polyglot](https://github.com/sheerun/vim-polyglot), I didn't
+want to have to hunt down and find a ton of different VSCode extensions just to
+have vsnip snippets for the languages I work with. I want one easy installation
+that *doesn't* come with a bunch of bloat (a lot of those extensions have images
+or even videos in their repo). This repo has *only* the snippet files for a
+range of languages.
+
+This repository is in its early days, and I've only added extensions for a few
+languages that I use most often. If your favorite language is missing, please
+submit a pull request with the missing extension (see
+[Contributing](#contributing) below).
+
+## Installation
+Supports any vim package manager, e.g. [Pathogen](https://github.com/tpope/vim-pathogen), [Vundle](https://github.com/VundleVim/Vundle.vim), [NeoBundle](https://github.com/Shougo/neobundle.vim), or [Plug](https://github.com/junegunn/vim-plug).
+
+## Languages
+* csharp - [vscode-csharp-snippets](https://github.com/J0rgeSerran0/vscode-csharp-snippets.git), [vscode-unity-code-snippets](https://github.com/kleber-swf/vscode-unity-code-snippets.git)
+* go - [vscode-go](https://github.com/golang/vscode-go.git)
+* html - [php-awesome-snippets](https://github.com/h4kst3r/php-awesome-snippets.git)
+* javascript - [vscode-es7-javascript-react-snippets](https://github.com/dsznajder/vscode-es7-javascript-react-snippets.git)
+* lua - [vsc-lua](https://github.com/keyring/vsc-lua.git)
+* php - [php-awesome-snippets](https://github.com/h4kst3r/php-awesome-snippets.git)
+* python - [vscode-Python-Extended](https://github.com/tushortz/vscode-Python-Extended.git)
+* rust - [vscode-rust](https://github.com/rust-lang/vscode-rust.git)
+* typescript - [vscode-es7-javascript-react-snippets](https://github.com/dsznajder/vscode-es7-javascript-react-snippets.git)
+
+## Contributing
+All snippets are generated programmatically. To add new extensions:
+
+* Edit `sources.json` 
+* Run the `build.py` script
+* Open a pull request
+
+## License
+
+The build script and all other code unique to this repository are under the MIT
+license (see LICENSE file). All snippets are under the specific license of their
+source repository. Each of these is in a subdirectory under `snippets/`, and
+their applicable license(s) are present in that subdirectory.
---
+++ build.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+import argparse
+import concurrent.futures
+import itertools
+import json
+import os
+import re
+import shutil
+import subprocess
+from collections import defaultdict
+from os.path import join
+from typing import Dict, List, Optional, Tuple
+
+here = os.path.dirname(__file__)
+build_dir = join(here, "build")
+snippet_dir = join(here, "snippets")
+
+
+def main():
+    """ Build the snippets from their source repos """
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument(
+        "-u",
+        "--update",
+        help="repo to update, or 'all' to update all source repos",
+    )
+    parser.add_argument(
+        "-p", type=int, help="Number of threads (default %(default)s)", default=5
+    )
+    args = parser.parse_args()
+
+    os.makedirs(build_dir, exist_ok=True)
+    with open(join(here, "sources.json"), "r") as ifile:
+        sources = json.load(ifile)
+    lockfile_path = join(here, "sources.lock")
+    if os.path.isfile(lockfile_path):
+        with open(lockfile_path, "r") as ifile:
+            lock_data = json.load(ifile)
+    else:
+        lock_data = {}
+
+    def execute(config):
+        should_update = args.update is not None and (
+            args.update == "all" or config["url"].endswith(args.update)
+        )
+        rev = None if should_update else lock_data.get(config["url"])
+        return build_source(config, rev)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=args.p) as executor:
+        results = executor.map(execute, sources)
+
+    all_snippets = []
+    packages_by_language = defaultdict(set)
+    lock_data = {}
+    for config, (git_rev, snippets) in zip(sources, results):
+        all_snippets.extend(snippets)
+        lock_data[config["url"]] = git_rev
+        for snip in snippets:
+            packages_by_language[snip["language"]].add(config["url"])
+
+    package_data = {"contributes": {"snippets": all_snippets}}
+    with open(join(here, "package.json"), "w") as package_file:
+        json.dump(package_data, package_file, indent=2)
+
+    with open(lockfile_path, "w") as ofile:
+        json.dump(lock_data, ofile, indent=2)
+
+    update_readme(packages_by_language)
+
+
+def build_source(config: str, rev: Optional[str] = None) -> Tuple[str, List[Dict]]:
+    dirname = url_basename(config["url"])
+    dirpath = join(build_dir, dirname)
+    try:
+        if os.path.isdir(dirpath):
+            if rev is None:
+                subprocess.check_call(
+                    ["git", "pull"],
+                    cwd=dirpath,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+        else:
+            subprocess.check_call(
+                ["git", "clone", config["url"]],
+                cwd=build_dir,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+    except Exception as e:
+        raise Exception(f"Error cloning or updating {dirname}")
+
+    if rev is not None:
+        try:
+            subprocess.check_call(
+                ["git", "checkout", rev],
+                cwd=dirpath,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+        except Exception as e:
+            raise Exception(f"Error checking out repo {dirname} rev {rev}")
+
+    rev = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=dirpath)
+        .decode("utf-8")
+        .strip()
+    )
+
+    with open(join(dirpath, "package.json"), "r") as package:
+        package_data = json.load(package)
+    snippets = package_data.get("contributes", {}).get("snippets", [])
+    if not snippets:
+        raise Exception(f"Source {dirname} is missing snippets in package.json")
+
+    dest_dir = join(snippet_dir, dirname)
+    os.makedirs(dest_dir, exist_ok=True)
+    for license in get_licenses(dirpath, config):
+        license_path = join(dirpath, license)
+        if not os.path.isfile(license_path):
+            raise Exception(f"Source {dirname} missing license {license}")
+        shutil.copyfile(license_path, join(dest_dir, license))
+
+    language_remap = config.get("remap_language", {})
+    our_snippets = []
+    for data in snippets:
+        language = language_remap.get(data["language"], data["language"])
+        path = join(dirpath, data["path"])
+        basename = os.path.basename(data["path"])
+        destpath = join(dest_dir, basename)
+        shutil.copyfile(path, destpath)
+        our_snippets.append(
+            {"language": language, "path": str(os.path.relpath(destpath, here))}
+        )
+    return rev, our_snippets
+
+
+def get_licenses(dirpath: str, config: dict) -> List[str]:
+    if "license" in config:
+        if isinstance(config["license"], str):
+            return [config["license"]]
+        else:
+            return config["license"]
+    else:
+        licenses = [f for f in os.listdir(dirpath) if re.match(r"LICENSE.*", f)]
+        if len(licenses) == 1:
+            return licenses
+        else:
+            return ["LICENSE.md"]
+
+
+def url_basename(url: str) -> str:
+    basename = url
+    if "/" in basename:
+        basename = basename[basename.rindex("/") + 1 :]
+    if basename.endswith(".git"):
+        basename = basename[:-4]
+    return basename
+
+
+def update_readme(packages_by_language: dict) -> None:
+    # These are not real languages
+    packages_by_language.pop("javascriptreact", None)
+    packages_by_language.pop("typescriptreact", None)
+
+    readme_file = join(here, "README.md")
+    prefix_lines = []
+    postfix_lines = []
+    readme_lines = prefix_lines
+    with open(readme_file, "r") as ifile:
+        language_section = False
+        for line in ifile:
+            if language_section:
+                if line.startswith("#"):
+                    language_section = False
+                    readme_lines = postfix_lines
+                    readme_lines.append(line)
+            else:
+                if line.startswith("## Languages"):
+                    language_section = True
+                readme_lines.append(line)
+
+    language_lines = []
+    for language, repos in sorted(packages_by_language.items()):
+        links = [f"[{url_basename(url)}]({url})" for url in repos]
+        language_lines.append(f"* {language} - {', '.join(links)}\n")
+
+    all_lines = prefix_lines + language_lines + ["\n"] + postfix_lines
+    with open(readme_file, "w") as ofile:
+        ofile.write("".join(all_lines))
+
+
+if __name__ == "__main__":
+    main()
